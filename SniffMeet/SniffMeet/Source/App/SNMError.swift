ce989da87184e3566df3055d6cd3c1039bcb6b56
Kelly-Chui <k.jinwon.choi@gmail.com>
2025-01-09 15:33:27 +0900

    [refactor] #2 SNMError 타입 변경
    - 너무 많은 케이스가 있어서 enumeration에서 struture로 타입을 변경했습니다.
    
    Co-Authored-By: Hyemin Heo <97100404+soletree@users.noreply.github.com>

--- SniffMeet/SniffMeet/Source/App/SNMError.swift
+++ SniffMeet/SniffMeet/Source/App/SNMError.swift
@@ -7,16 +7,15 @@
 
 import Foundation
 
-enum SNMError: LocalizedError {
-    case user(error: any Error)
-    case developer(error: any Error)
-    case fatal(error: any Error)
+struct SNMError: LocalizedError {
+    enum ErrorLevel: String {
+        case user = "유저"
+        case developer = "개발자"
+    }
+    let level: ErrorLevel
+    let error: any Error
     
     var errorDescription: String? {
-        switch self {
-        case .user(let error): "유저 레벨 에러 \(error.localizedDescription) 발생"
-        case .developer(let error): "개발자 레벨 에러 \(error.localizedDescription) 발생"
-        case .fatal(let error): "치명적인 에러 \(error.localizedDescription) 발생"
-        }
+        "\(level.rawValue) 레벨 에러 \(error.localizedDescription) 발생"
     }
 }
