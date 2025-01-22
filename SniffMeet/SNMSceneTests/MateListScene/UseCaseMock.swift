9c8634ed0251062a324ddc0b87492c1ab80c94b5
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2025-01-22 16:58:45 +0900

    [test] #28 MateListInteractor에 포함되는 UseCase 대상으로 Mock 객체 생성

---
+++ SniffMeet/SNMSceneTests/MateListScene/UseCaseMock.swift
@@ -0,0 +1,63 @@
+//
+//  UseCase.swift
+//  SniffMeet
+//
+//  Created by 윤지성 on 1/22/25.
+//
+import Combine
+import Foundation
+import UIKit
+
+struct RequestMateListUseCaseMock: RequestMateListUseCase {
+    var remoteDatabaseManager: any RemoteDatabaseManager
+    var mateList: [UserInfoDTO]
+    
+    init(mateList: [UserInfoDTO]) {
+        remoteDatabaseManager = RemoteDatabaseManagerMock(fetchData: nil, fetchListData: nil)
+        self.mateList = mateList
+    }
+    
+    func execute() async -> [Mate] {
+        mateList.map{
+            Mate(name: $0.dogName,
+                 userID: $0.id,
+                 keywords: $0.keywords,
+                 profileImageURLString: $0.profileImageURL)
+        }
+    }
+}
+
+struct RequestProfileImageUseCaseMock: RequestProfileImageUseCase {
+    func execute(fileName: String) async -> Data? {
+        UIImage.checkmark.pngData()
+    }
+}
+
+final class TryProfileDropUseCaseMock: TryProfileDropUseCase {
+    var profilePublisher: CurrentValueSubject<DogDTO?, Never> = CurrentValueSubject(nil)
+    var isNIConnected: CurrentValueSubject<Bool, Never> = CurrentValueSubject(false)
+    var transmissionFlag: Set<String> = []
+    var isTransistioned: Bool = false
+    var triedBefore: Bool = false
+    
+    init() {
+        
+    }
+    
+    func execute() {
+    }
+    
+    func loadProfileData() {
+    }
+    
+    func reset(mpcManager: MPCManager, nimanager: NIManager) {
+    }
+}
+
+struct QuitProfileDropUseCaseMock: QuitProfileDropUseCase {
+    func execute() {
+    }
+    
+    mutating func reset(niManager: NIManager) {
+    }
+}
